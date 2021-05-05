2ea201fdbbe829b08d2fb6d8bf35e098e844824b
Andrew High <andrewhigh4@hotmail.com>
2021-05-05 19:35:32 +0100

    3 problems solved

--- hw_advanced_start/isogram_finder/isogram_finder.js
+++ hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,18 @@
 const IsogramFinder = function (word) {
-
+    this.wordLowerCase = word.toLowerCase();
+    this.wordSplit = this.wordLowerCase.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    const uniqueLetters = [];
+    const answer = this.wordSplit.every(wordLetter => {
+        let check = uniqueLetters.includes(wordLetter);
+        if (check === false){
+            uniqueLetters.push(wordLetter);
+        };
+        return !check        
+    });
+    return answer;
 }
 
 module.exports = IsogramFinder;
