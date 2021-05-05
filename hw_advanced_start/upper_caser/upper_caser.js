7cd5ae92d8aa9e12aaef2694a3363652ea58dc02
Andrew High <andrewhigh4@hotmail.com>
2021-05-05 17:29:47 +0100

    first problem solved

--- hw_advanced_start/upper_caser/upper_caser.js
+++ hw_advanced_start/upper_caser/upper_caser.js
@@ -1,8 +1,10 @@
 const UpperCaser = function (words) {
-}
+    this.words = words;
+};
 
 UpperCaser.prototype.toUpperCase = function () {
-
-}
+   const upperCaseWords = this.words.map(word => word.toUpperCase());
+   return upperCaseWords;
+};
 
 module.exports = UpperCaser;
