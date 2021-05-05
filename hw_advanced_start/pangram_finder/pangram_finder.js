96db65c8c6acba2b86c7530afcc020e914b286a6
Andrew High <andrewhigh4@hotmail.com>
2021-05-05 17:56:47 +0100

    second problem solved

--- hw_advanced_start/pangram_finder/pangram_finder.js
+++ hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,14 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phraseLowerCase = phrase.toLowerCase();
+  this.phraseSplit = this.phraseLowerCase.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  const answer = this.alphabet.every(alphabetLetter => {
+    return this.phraseSplit.some(phraseLetter => phraseLetter === alphabetLetter)
+  });
+  return answer;
+};
 
 module.exports = PangramFinder;
